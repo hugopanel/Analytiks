181a352a3c4032142b6eef54986098805ef1a94b
Hugo PANEL <hugopanel@gmail.com>
2024-07-03 15:58:12 +0200

    Added a route for event streaming

--- Api/main.py
+++ Api/main.py
@@ -1,42 +1,68 @@
+import asyncio
+import json
+import logging
+import time
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from kafka import KafkaConsumer, TopicPartition, KafkaProducer
+from aiokafka import AIOKafkaConsumer
 
 app = FastAPI()
 
 # Setup Kafka
-## Consumer
-consumer = KafkaConsumer(bootstrap_servers='localhost:9092', enable_auto_commit=False, api_version="2.7.0")
 topic = 'events'
-partition = 0
-tp = TopicPartition(topic, partition)
-consumer.assign([tp])
-consumer.seek_to_beginning(tp) # Start from the beginning
 
-## Producer
 producer = KafkaProducer(bootstrap_servers='localhost:9092', api_version="2.7.0")
+
+## Add PySpark and connect it to the kafka topic
+# import findspark
+# findspark.init()
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import from_json, col
+# from pyspark.sql.types import StructType, StringType
+
+# spark = SparkSession.builder.appName("Analytiks").getOrCreate()
+# schema = StructType().add("event", StringType()).add("value", StringType())
+# df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", topic).load()
+# df = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+# df = df.select(from_json(col("value"), schema).alias("data")).select("data.*")
+
 
 # Setup API routes
 @app.get("/")
 def read_root():
     return {"name": "Analytiks API", "version": "1"}
 
+
 @app.get("/register_event/{event}/{value}")
-def register_event(event: str, value: str = None):
+async def register_event(event: str, value: str = None):
     producer.send(topic, key=event.encode(), value=value.encode())
     producer.flush()
     return {"event": event, "value": value} 
 
-"""
-This route will consume messages from the Kafka topic and return them as a list of dictionaries.
-THIS ROUTE IS ONLY SUPPOSED TO BE USED FOR TESTING PURPOSES.
-nb_messages must be AT MOST equal to the number of mesaages in the topic. Otherwise, it will loop until there are *nb_messages* messages.
-"""
-@app.get("/get_events/{nb_messages}")
-def get_events(nb_messages: int = 10):
-    events = []
-    # Limit the number of iterations
-    for i, message in enumerate(consumer):
-        events.append({message.key.decode(): message.value.decode()})
-        if i >= nb_messages:  # Change the number of iterations as needed
-            break
-    return events
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Helper function for the /get_events_stream route
+async def consume_messages():
+    consumer = AIOKafkaConsumer(
+        topic,
+        bootstrap_servers='localhost:9092',
+        group_id='mygroup'
+    )
+
+    await consumer.start()
+    logger.info("Kafka consumer started")
+    try:
+        async for message in consumer:
+            yield json.dumps({"type": message.key.decode(), "subject": message.value.decode()}) + "\n"
+    finally:
+        logger.info("Stopping Kafka consumer")
+        await consumer.stop()
+
+# Route to stream the events
+@app.get("/get_events_stream", response_class=StreamingResponse)
+async def stream():
+    return StreamingResponse(consume_messages(), media_type="application/x-ndjson")
